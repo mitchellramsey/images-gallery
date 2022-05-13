776aff92d8f6c1ff55a00aa0febe885047148406
Mitchell Ramsey <mitchellramsey1992@gmail.com>
2022-05-13 12:33:23 -0400

    hooked up unspalsh api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,30 @@
+import { useState } from 'react';
+import Header from './components/Header';
+import Search from './components/Search';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_APP_KEY
 
 function App() {
+  const [word, setWord] = useState('')
+
+  const handleSearchSubmit = async (e) => {
+    try{
+      e.preventDefault()
+      const res = await fetch(`https://api.unsplash.com/photos/random?query=${word}&client_id=${UNSPLASH_KEY}`)
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+
+
   return (
     <div className="App">
-      <h1>Images Gallery</h1>
+      <Header title='Images Gallery'/>
+      <Search handleSubmit={handleSearchSubmit} word={word} setWord={setWord}/>
     </div>
   );
 }
