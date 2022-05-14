19cb10542779109ea875f08e24e22170e3ec7778
Mitchell Ramsey <mitchellramsey1992@gmail.com>
2022-05-14 11:22:02 -0400

    created first version of flask app

---
+++ api/main.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  return 'Hello World!'
+
+@app.route("/myRoute")
+def goodbye():
+  return 'Good Bye!'
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", port=5050)
+  
