bf5a200acde7e59af0a403cb9bcad77f8b4eed5d
Mitchell Ramsey <mitchellramsey1992@gmail.com>
2022-05-15 11:53:45 -0400

    Enabled python linter and formatter

--- api/main.py
+++ api/main.py
@@ -1,38 +1,35 @@
+""" Backend application for Images Gallery application """
+import os
+import requests
 from flask import Flask, request
 from flask_cors import CORS
 from dotenv import load_dotenv
-import requests
-import os
 
-load_dotenv(dotenv_path='./.env.local')
 
-UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
-UNSPLASH_KEY = os.environ.get('UNSPLASH_APP_KEY', 'abc')
-DEBUG = bool(os.environ.get('DEBUG', True))
+load_dotenv(dotenv_path="./.env.local")
+
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_APP_KEY", "abc")
+DEBUG = bool(os.environ.get("DEBUG", True))
 
 if not UNSPLASH_KEY:
-    raise EnvironmentError('Please create .env.local file and insert UNSPLASH_API_KEY')
+    raise EnvironmentError("Please create .env.local file and insert UNSPLASH_API_KEY")
 
 app = Flask(__name__)
 CORS(app)
 
-app.config['DEBUG'] = DEBUG
+app.config["DEBUG"] = DEBUG
+
 
 @app.get("/new-image")
 def new_image():
+    """Grabs and returns random image from Unsplash API with provided query"""
     word = request.args.get("query")
-    headers = {
-        'Authorization': 'Client-ID ' + UNSPLASH_KEY,
-        'Accept-Version': 'v1' 
-    }
-    payload = {
-        'query': word
-    }
-    r = requests.get(url=UNSPLASH_URL, headers=headers, params=payload)
-    return r.json()
+    headers = {"Authorization": "Client-ID " + UNSPLASH_KEY, "Accept-Version": "v1"}
+    payload = {"query": word}
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=payload)
+    return response.json()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
-
-
